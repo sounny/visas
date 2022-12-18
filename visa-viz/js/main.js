97caa0a12a6bdea26e2cee5fcb55f3c9ac4ba988
dbabsoniv <dbabsoniv@gmail.com>
2022-12-17 18:13:37 -0800

    6:13 appending pie chart

--- visa-viz/js/main.js
+++ visa-viz/js/main.js
@@ -89,6 +89,8 @@ function setWorldMap(){
         //add enum units to the map
         setWorldEnumerationUnits(countriesFeature, map, path, colorScale);
 
+        setPieChart();
+
     };
 
     
@@ -534,5 +536,37 @@ function isDestinationSelected(){
 }
 
 
-/// WIPE destination when changing origin
-/// ENABLE destinationSelect when changing origin, if NOT to default (then set it to disabled) 
+
+function setPieChart() {
+    var data = [2, 4, 8, 10];
+
+    var svg = document.getElementById("piechart"),
+        width = svg.attr("width"),
+        height = svg.attr("height"),
+        radius = Math.min(width, height) / 2,
+        g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c']);
+
+    // Generate the pie
+    var pie = d3.pie();
+
+    // Generate the arcs
+    var arc = d3.arc()
+                .innerRadius(0)
+                .outerRadius(radius);
+
+    //Generate groups
+    var arcs = g.selectAll("arc")
+                .data(pie(data))
+                .enter()
+                .append("g")
+                .attr("class", "arc")
+
+    //Draw arc paths
+    arcs.append("path")
+        .attr("fill", function(d, i) {
+            return color(i);
+        })
+        .attr("d", arc);
+};
