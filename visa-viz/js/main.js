e61752de33577c573eca51488f056ea975869acb
dbabsoniv <dbabsoniv@gmail.com>
2022-12-17 18:36:07 -0800

    6:36

--- visa-viz/js/main.js
+++ visa-viz/js/main.js
@@ -544,7 +544,9 @@ function setPieChart() {
         width = svg.getAttribute("width"),
         height = svg.getAttribute("height"),
         radius = Math.min(width, height) / 2,
-        g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    
+    var g = svg.append("g")
+               .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
     var color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c']);
 
